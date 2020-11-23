b5c6a590f8b85e316cd54be54609e2179eb092a3
Teodor Radu <teodor.rd27@gmail.com>
2020-11-23 17:18:21 +0000

    seed the categories as well, change ids to objectids

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -3,3 +3,4 @@
 mongoimport --host mongodb --db challenge --collection users --type json --file /mongo-seed/mongo-seed/user.json --jsonArray
 mongoimport --host mongodb --db challenge --collection campaigns --type json --file /mongo-seed/mongo-seed/campaign.json --jsonArray
 mongoimport --host mongodb --db challenge --collection apps --type json --file /mongo-seed/mongo-seed/apps.json --jsonArray
+mongoimport --host mongodb --db challenge --collection categories --type json --file /mongo-seed/mongo-seed/categories.json --jsonArray
